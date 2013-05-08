2d6fd9bf9b3239659bf793eaace0fdb5bd4b0175
Brandon Rice <brice84@gmail.com>
2013-05-08 19:33:01 -0400

    Fix achievements endpoint for new gameid param.

--- lib/xbox_leaders/api.rb
+++ lib/xbox_leaders/api.rb
@@ -12,7 +12,7 @@ class XboxLeaders::Api
   end
 
   def fetch_achievements(gamertag, game_id)
-    get('/achievements', gamertag: gamertag, titleid: game_id)
+    get('/achievements', gamertag: gamertag, gameid: game_id)
   end
 
   def fetch_friends(gamertag)
